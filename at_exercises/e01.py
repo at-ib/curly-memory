ace0b6b46803a78cbc4e02136b3d8f82a09343d6
at-ib <10222743+at-ib@users.noreply.github.com>
2024-01-06 21:23:17 +0530

    Start exercises

---
+++ at_exercises/e01.py
@@ -0,0 +1,45 @@
+# 1. Machine learning is programming computers to be able to learn from data to do tasks without being explicitly told
+# how to do it
+
+# 2. Image classification, film recommendation, anomaly detection, LLMs
+
+# 3. A labelled training set is when each data point in the training set has a label which is the answer that the
+# algorithm being trained should aim to predict
+
+# 4. Classification and regression
+
+# 5. Clustering, anomaly detection, novelty detection, visualisation
+
+# 6. Reinforcement learning
+
+# 7. Clustering
+
+# 8. Supervised
+
+# 9. A system which can learn from new data incrementally
+
+# 10. Learning in which not all the data is held in memory at once.
+
+# 11. Instance based learning
+
+# 12. A model parameter is learned during training, a hyperparameter is set before training and controls how the
+# algorithm learns
+
+# 13. Model based algorithms search for parameters of a statistical model. The most common strategy they use is to maximise
+# a utility function. They generate predictions using the model.
+
+# 14. Overfitting, underfitting, not enough training data, biased data
+
+# 15. Overfitting. Try using a simpler model, more training data, reducing the noise in the data by cleaning it up
+
+# 16. A test set is a subset of the total data reserved for testing. This means the effectiveness of the model can be
+# assessed on data that wasn't involved in the training
+
+# 17. The validation set is used to select the best model and tune the hyperparameters
+
+# 18. When training data is scarce, but something similar and abundant is available, the similar data can be used for
+# training instead. In this case it is split into train and train-dev. Train-dev is used for validation.
+
+# 19. The model can perform poorer than expected once deployed.
+
+
---
+++ at_exercises/e02_01.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import numpy
+import pandas
+import pandas as pd
+import tarfile
+import urllib.request
+
+import sklearn.model_selection
+from sklearn.impute import SimpleImputer
+
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+
+housing = load_housing_data()
+
+
+def get_data_splits(housing_data):
+    return sklearn.model_selection.train_test_split(housing_data, train_size=5000, random_state=0)
+
+
+def prepare_data(data):
+    data = impute_missing_values(data)
+    data = encode_categoricals(data)
+    return data
+
+
+def impute_missing_values(data):
+    imputer = SimpleImputer(strategy="median")
+    data_num = data.select_dtypes(include=numpy.number)
+    data_nonnum = data.select_dtypes(exclude=numpy.number)
+    data_num = imputer.fit_transform(data_num)
+    return pandas.concat([data_num, data_nonnum], axis=1)
+
+
+def encode_categoricals(data):
+    return data
