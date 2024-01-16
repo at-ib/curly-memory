3899ad0f195cae4e4a639834a5be9e789f545163
at-ib <10222743+at-ib@users.noreply.github.com>
2024-01-16 11:47:17 +0530

    Chapter 4 exercises

--- at_exercises/e03.py
+++ at_exercises/e03.py
@@ -1,15 +1,23 @@
 import pickle
+import tarfile
+from pathlib import Path
 
+import numpy
 import pandas
 from matplotlib import pyplot
+from scipy.ndimage import shift
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 
-def get_data():
-    with open("data/mnist.pkl", "rb") as f:
+def get_mnist_data():
+    with open("datasets/mnist.pkl", "rb") as f:
         mnist = pickle.load(f)
     return mnist
 
@@ -27,7 +35,7 @@ def get_train_test_data(mnist):
 
 
 def q1():
-    mnist = get_data()
+    mnist = get_mnist_data()
     data, test_data, labels, test_labels = get_train_test_data(mnist)
     knn_clf = Pipeline([("knn", KNeighborsClassifier())])
     param_grid = [
@@ -42,9 +50,101 @@ def q1():
 
 
 def q1_test():
-    mnist = get_data()
+    mnist = get_mnist_data()
     data, test_data, labels, test_labels = get_train_test_data(mnist)
     knn_clf = Pipeline([("knn", KNeighborsClassifier(n_neighbors=4, weights="distance"))])
     knn_clf.fit(data, labels)
     test_predictions = knn_clf.predict(test_data)
     return accuracy_score(test_predictions, test_labels)
+
+
+def q2():
+    mnist = get_mnist_data()
+    data, test_data, labels, test_labels = get_train_test_data(mnist)
+    data, labels = augment_data(data, labels)
+    knn_clf = Pipeline([("knn", KNeighborsClassifier(n_neighbors=4, weights="distance"))])
+    knn_clf.fit(data, labels)
+    test_predictions = knn_clf.predict(test_data)
+    return accuracy_score(test_predictions, test_labels)
+
+
+def augment_data(data, labels):
+    data = [im.reshape((28, 28)) for im in data]
+    data = (
+        [
+            shift(im, shift_value, mode="constant", cval=0)
+            for shift_value in [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1)]
+            for im in data
+        ]
+    )
+    data = numpy.array([im.reshape((28 * 28, )) for im in data])
+    labels = numpy.tile(labels, 5)
+    return data, labels
+
+
+def q3():
+    # Initial thoughts about the data
+    # Pclass is social class rather than cabin class, this could be important
+    # Name probably isn't important, but I could 1 hot encode surname, to see if there's correlation between families
+    # Actually this is covered by SibSp and Parch, so probably drop name
+    # Sex is probably important
+    # Age is probably important
+    # SibSp is number of siblings and spouses, having this means I can probably drop name
+    # Parch is the number of parents and children
+    # Ticket is probably irrelevant. I don't want to go too deep on it so drop.
+    # Cabin is probably very relevant. I could separate the deck from the number as 2 features, but it's very
+    # incomplete so ignore for now.
+    # Embarked is C = Cherbourg, Q = Queenstown, S = Southampton. Probably not relevant unless it determines something
+    # else about the passengers. One hot encode.
+
+    data, test_data, labels = load_titanic_data()
+    preprocessing = get_preprocessing_pipeline()
+    rf_pipeline = Pipeline(
+        [
+            ("preprocessing", preprocessing),
+            ("rf", RandomForestClassifier(random_state=0))
+        ]
+    )
+    param_grid = [
+        {
+            'rf__criterion': ["gini", "entropy", "log_loss"],
+            "rf__max_depth": [None, 10, 100, 1000],
+            "rf__min_weight_fraction_leaf": [0.0, 0.1, 0.2]
+         },
+    ]
+    search = RandomizedSearchCV(
+        rf_pipeline,
+        param_grid,
+        cv=5,
+        scoring='accuracy',
+        random_state=0,
+        n_iter=10
+    )
+    search.fit(data, labels)
+    return pandas.DataFrame(search.cv_results_)
+
+
+def get_preprocessing_pipeline():
+    cat_pipeline = make_pipeline(
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder(handle_unknown="ignore")
+    )
+    num_pipeline = make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
+    return ColumnTransformer(
+        [
+            ("num", num_pipeline, ["Pclass", "Age", "SibSp", "Parch", "Fare"]),
+            ("cat", cat_pipeline, ["Sex", "Embarked"]),
+        ],
+        remainder="drop"
+    )
+
+
+def load_titanic_data():
+    tarball_path = Path("datasets/titanic.tgz")
+    with tarfile.open(tarball_path) as tarball:
+        tarball.extractall(path="datasets")
+    train = pandas.read_csv(Path("datasets/titanic/train.csv"))
+    y_train = train["Survived"]
+    x_train = train.drop(columns="Survived")
+    x_test = pandas.read_csv(Path("datasets/titanic/test.csv"))
+    return x_train, x_test, y_train
---
+++ at_exercises/e04.py
@@ -0,0 +1,110 @@
+# 1. Gradient descent
+
+# 2. Gradient descent suffers with mismatched scales. Standardizing features helps with this.
+
+# 3. No, because the cost function is convex
+
+# 4. No. Stochastic gradient descent probably won't reach the optimum, it will just get close
+
+# 5. The learning rate is too high
+
+# 6. No, because with mini batch you sometimes go uphill in a single step
+
+# 7. The algorithm that will get to the vicinity of the optimum first is stochastic? Batch gradient descent will
+# actually converge. Others can be made to converge by reducing the learning rate over time.
+
+# 8. If there is a large gap between the learning curves for the training and validation data then the model is
+# overfitting. This can be improved with more data, more regularization or a simpler model.
+
+# 9. If training and validation error are both high the model is underfitting (ie high bias). In which case we want less
+# regularization. For ridge regression, this means we should reduce alpha.
+
+# 10a. Use ridge regression to constrain the weights of the model and therefore reduce overfitting.
+
+# 10b. Use lasso regression when you suspect only a few features are important because it tends to set most of the
+# weights to zero
+
+# 10c. Elastic net is a compromise between ridge regression and lasso regression. In general, elastic net is preferred
+# over lasso because lasso may behave erratically when the number of features is greater than the number of training
+# instances or when several features are strongly correlated.
+
+# 11. 2 logistic regression classifiers. Softmax classifiers are not multioutput.
+import numpy
+from numpy import dot, hstack, exp, arange, argmax, random
+from numpy.random import rand, randn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
+
+
+def q12():
+    random.seed(1)
+    iris = load_iris(as_frame=True)
+    data, test_data, labels, test_labels = train_test_split(
+        iris.data,
+        iris.target,
+        random_state=0,
+        stratify=iris.target
+    )
+    theta_initial = randn(5, 3)
+    learning_rate = 0.1
+    tol = 0.00001
+    return gradient_descent(theta_initial, data, labels, learning_rate, tol)
+
+
+def predict(theta, x):
+    # x_i is the ith training instance
+    return argmax([softmax(k, theta, x) for k in arange(theta.shape[1])])
+
+
+def softmax(k, theta, x):
+    num = exp(score(k, theta, x))
+    den = numpy.sum([exp(score(j, theta, x)) for j in arange(theta.shape[1])])
+    return num / den
+
+
+def score(k, theta, x):
+    return dot(theta[:, k], hstack((1, x)))
+
+
+def cost(theta, data, labels):
+    num = numpy.sum([numpy.log(softmax(labels.iloc[i], theta, data.iloc[i, :])) for i in numpy.arange(len(data))])
+    den = -len(data)
+    return num / den
+
+
+def gradient(theta, data, labels):
+    k_range = numpy.arange(theta.shape[1])
+    return numpy.array([gradient_k(k, theta, data, labels) for k in k_range]).T
+
+
+def gradient_k(k, theta, data, labels):
+    m_range = numpy.arange(len(data))
+    num = numpy.sum(
+        [(softmax(k, theta, data.iloc[i, :]) - indicator(k, i, labels)) * hstack((1, data.iloc[i, :])) for i in m_range],
+        axis=0
+    )
+    den = len(data)
+    return num / den
+
+
+def indicator(k, i, labels):
+    return int(labels.iloc[i] == k)
+
+
+def theta_next(theta, data, labels, learning_rate):
+    return theta - learning_rate * gradient(theta, data, labels)
+
+
+def gradient_descent(theta_initial, data, labels, learning_rate, tol):
+    prev_cost = cost(theta_initial, data, labels)
+    new_cost = prev_cost - 2 * tol
+    theta = theta_initial
+    i = 0
+    while new_cost <= prev_cost:
+        print(f"step_{i}")
+        prev_cost = new_cost.copy()
+        theta = theta_next(theta, data, labels, learning_rate)
+        new_cost = cost(theta, data, labels)
+        print(f"cost: {new_cost}")
+        i += 1
+    return theta
