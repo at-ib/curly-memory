fcee93ca0e868deec9308e190568f4f4774696e8
at-ib <10222743+at-ib@users.noreply.github.com>
2024-01-22 19:23:58 +0530

    Finish exercises 7

--- at_exercises/e07.py
+++ at_exercises/e07.py
@@ -17,8 +17,10 @@
 # strongly regularizing the base estimator.
 
 # 7. If gradient boosting algorithm overfits reduce the learning_rate hyperparameter
+import numpy
 from sklearn.datasets import fetch_openml
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier, StackingClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
@@ -48,3 +50,45 @@ def q8():
     voting_clf.fit(data, labels)
     print(f"voting_clf accuracy on validation set: {voting_clf.score(validation_data, validation_labels)}")
 
+
+def q9():
+    mnist = fetch_openml('mnist_784', as_frame=False, parser="auto")
+    data, test_data, labels, test_labels = train_test_split(mnist.data, mnist.target, test_size=10000, random_state=0)
+    data, validation_data, labels, validation_labels = train_test_split(data, labels, test_size=10000, random_state=0)
+    rf_clf = RandomForestClassifier(random_state=0)
+    rf_clf.fit(data, labels)
+    et_clf = ExtraTreesClassifier(random_state=0)
+    et_clf.fit(data, labels)
+    sv_clf = SVC(random_state=0)
+    sv_clf.fit(data, labels)
+    layer_0_predictions = (
+        rf_clf.predict(validation_data),
+        et_clf.predict(validation_data),
+        sv_clf.predict(validation_data),
+    )
+    blender_data = numpy.concatenate([pred.reshape(-1, 1) for pred in layer_0_predictions], axis=1)
+    rf_blender = RandomForestClassifier(random_state=1)
+    rf_blender.fit(blender_data, validation_labels)
+    blender_test_predictions = get_blender_predictions(test_data, [rf_clf, et_clf, sv_clf], rf_blender)
+    print(f"Blender accuracy: {accuracy_score(blender_test_predictions, test_labels)}")
+
+
+def get_blender_predictions(data, layer_0, blender):
+    layer_0_predictions = (clf.predict(data) for clf in layer_0)
+    layer_0_predictions = numpy.concatenate([pred.reshape(-1, 1) for pred in layer_0_predictions], axis=1)
+    return blender.predict(layer_0_predictions)
+
+
+def q9_stacking_classifier():
+    mnist = fetch_openml('mnist_784', as_frame=False, parser="auto")
+    data, test_data, labels, test_labels = train_test_split(mnist.data, mnist.target, test_size=10000, random_state=0)
+    stacking_clf = StackingClassifier(
+        estimators=[
+            ('rf', RandomForestClassifier(random_state=0)),
+            ('et', ExtraTreesClassifier(random_state=0)),
+            ('svc', SVC(random_state=0))
+        ],
+        final_estimator=RandomForestClassifier(random_state=1),
+    )
+    stacking_clf.fit(data, labels)
+    print(f"StackingClassifier accuracy: {stacking_clf.score(test_data, test_labels)}")
